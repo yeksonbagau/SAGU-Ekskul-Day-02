6236dd4ff8092e351ce815d4870f47b918cf07a8
yeksonbagau <somatuaubagau@gmail.com>
2022-02-26 11:32:19 +0900

    push project03

---
+++ project03/comments.php
@@ -0,0 +1,11 @@
+<?php
+    
+    //this is my first sigle line comment in PHP
+    echo "single line comment using php '//' <br><br>";
+
+    #this is also a single line comment
+    echo "sigle line comment using'#'";
+
+
+    /*This is multipe line comment in php*/
+?>
