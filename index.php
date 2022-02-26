e63bc21df71d75bad0488b5b6c54ca9fcc1e5879
yeksonbagau <somatuaubagau@gmail.com>
2022-02-26 10:00:14 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAGU Ekskul Day 02</title>
+</head>
+<body>
+    <h1>
+        Sagu Ekskul day 02
+    </h1>
+</body>
+</html>
