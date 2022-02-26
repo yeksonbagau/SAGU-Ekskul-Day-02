e19b626d8d5aae0a032a7558e47c797f9907d660
yeksonbagau <somatuaubagau@gmail.com>
2022-02-26 10:48:38 +0900

    push project01

---
+++ project01/php-and-html.php
@@ -0,0 +1,26 @@
+<?php 
+    
+    print("
+
+        <html>
+
+            <head>
+
+                <title>PHP and HTML</title>
+
+            </head>
+            
+            <body>
+
+                <h1>Hello SAGU</h1>
+                <p> We are learning PHP Codes.</p>
+
+            </body>
+
+        </html>    
+
+        
+    
+    ");
+
+?>
